29b43977951202b183808f517a9f8981dbe6b708
Andi Andreas <nexuister@gmail.com>
2019-10-19 22:26:14 -0400

    test #1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia()) {
   var video = document.getElementById("video");
   navigator.mediaDevices
     .getUserMedia({ video: { deviceId: { exact: "ygjrkJP80uMJUC2b+mh0nYTuvlf8lzO6AnRQl+Yan9w=" } } })
