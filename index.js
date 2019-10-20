753570f95906b5481a9e93807d321c16e87ec7e3
Andi Andreas <nexuister@gmail.com>
2019-10-19 22:41:54 -0400

    test #2

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia()) {
-  var video = document.getElementById("video");
-  navigator.mediaDevices
-    .getUserMedia({ video: { deviceId: { exact: "ygjrkJP80uMJUC2b+mh0nYTuvlf8lzO6AnRQl+Yan9w=" } } })
-    .then(function(stream) {
-      video.srcObject = stream;
-      video.play();
-    });
-}
+var video = document.getElementById("video");
+navigator.mediaDevices
+  .getUserMedia({ video: true })
+  .then(function(stream) {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(err => console.log(err));
